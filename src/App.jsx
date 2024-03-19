c710ae2e16fa4cd3bbf493eb8b91b03e2599b452
Bhanu-Prakash <Bhanu.Prakash@247.ai>
2024-03-19 10:25:29 +0530

    import fix for deploy

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/js/dist/dropdown";
-import PathfindingVisualizer from "./PathfindingVisualizer/pathfindingVisualizer";
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
 
 ReactDOM.render(
   <React.StrictMode>
