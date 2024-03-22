3598ee603b13c27d858dbff5a087286b7ec059bf
Bhanu Prakash <46644862+prabhanukash@users.noreply.github.com>
2024-03-22 07:05:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/js/dist/dropdown";
-import PathfindingVisualizer from "./pathfindingVisualizer/pathfindingVisualizer.jsx";
+import PathfindingVisualizer from "./pathfindingVisualizer/PathfindingVisualizer.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <PathfindingVisualizer />
   </React.StrictMode>,
-)
+  document.getElementById("root")
+);
